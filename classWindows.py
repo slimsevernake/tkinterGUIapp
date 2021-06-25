cef9c57abf108b35ebe411d105fd638bf259181d
Artem <furens@protonmail.com>
2021-06-25 11:55:52 +0300

    add class child_window

--- classWindows.py
+++ classWindows.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from child_window import ChildWindow
 
 class Window:
 
@@ -13,6 +14,9 @@ class Window:
     def run(self):
         self.root.mainloop()
 
+    def create_child(self, width, height, title='Child', resizable=(False, False), icon=None):
+        ChildWindow(self.root, width, height, title, resizable, icon)
 if __name__ == '__main__':
     window = Window(500, 500, 'Main')
+    window.create_child(200, 200)
     window.run()
